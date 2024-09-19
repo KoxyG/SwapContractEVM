35a518c546a11aa59b267c4e345607aeeb095eb4
koxy <devkoxy@gmail.com>
2024-09-19 13:17:12 +0100

    setting up tokens

---
+++ contracts/Tokens/GiftToken.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+
+contract GiftToken is ERC20 {
+
+    address public owner;
+
+    constructor() ERC20("GiftToken", "GT") {
+        owner = msg.sender;
+         _mint(msg.sender, 10000e18);
+
+    }
+
+    function mint(address to, uint256 amount) internal {
+        _mint(to, amount);
+    }
+
+}
